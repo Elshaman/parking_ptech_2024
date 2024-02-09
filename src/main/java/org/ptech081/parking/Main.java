903db70101a9b8d681d609f554bc1417fb76be82
Your Name <you@example.com>
2024-02-09 11:57:16 -0500

    Constructores sobrecargados

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -1,7 +1,29 @@
 package org.ptech081.parking;
+import org.ptech081.parking.entities.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       
+        //crear carros con los constructores:
+        Carro carrito1 = new Carro("ASB 456" ,
+                                 TipoVehiculo.MOTO);
+        Carro carrito2 = new Carro();
+        carrito2.placa = "ERRT 566";
+        carrito2.tipoVehiculo = TipoVehiculo.CAMIONETA;
+
+        //crear clientes
+        Cliente cliente1 = new Cliente("Cristian", 
+                                "Buitrago",
+                                 1243234553L);
+        //invocar el metodo addCar:
+        cliente1.addCar(carrito1);
+        cliente1.addCar("ASB 456" ,
+                   TipoVehiculo.BUS);
+        //recorrer los carros del cliente
+        for( Carro c :  cliente1.misCarros){
+            System.out.println(c.placa + " " + c.tipoVehiculo);
+            System.out.println("--------------------");
+        }
+
     }
 }
---
+++ src/main/java/org/ptech081/parking/entities/Cliente.java
@@ -0,0 +1,41 @@
+package org.ptech081.parking.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+
+    public String nombre;
+    public String apellidos;
+    public Long identificacion;
+    public List<Carro> misCarros;
+
+    public Cliente() {
+        this.misCarros = new ArrayList<Carro>();
+    }
+
+    public Cliente(String nombre, String apellidos, Long identificacion) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.identificacion = identificacion;
+        this.misCarros = new ArrayList<Carro>();
+    }
+
+    //primera sobrecarga de addCar
+    public void addCar(Carro car) {
+        this.misCarros.add(car);
+    }
+
+    //segunda sobrecarga de addCar
+    public void addCar(String placa,
+                       TipoVehiculo tv) {
+            Carro car = new Carro(placa , tv);
+            this.misCarros.add(car);
+    }
+
+
+
+    
+
+    
+}
